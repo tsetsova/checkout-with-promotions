335833476f9646fed56ad0ad40f9cacebdeb8e8e
Ina Tsetsova <inatsetsova@gmail.com>
2021-05-18 14:20:35 +0300

    total for multiple items

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -3,8 +3,10 @@ require 'checkout'
 describe Checkout do 
     let(:checkout) { described_class.new() }
 
-    it "scans an item" do
+    it "creates a total for multiple items" do
         checkout.scan({code: "001", name: "Very Cheap Chair", price: 9.25})
-        expect(checkout.total).to eq 9.25
+        checkout.scan({code: "002", name: "Little Table", price: 45.00})
+        checkout.scan({code: "003", name: "Funky Light", price: 19.95})
+        expect(checkout.total).to eq 74.20
     end 
 end 
