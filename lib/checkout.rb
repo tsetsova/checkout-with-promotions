444f2423ffe03fd029cdd43668eb8afbea6d5d83
Ina Tsetsova <inatsetsova@gmail.com>
2021-05-18 16:46:32 +0300

    use hash instead of array for better handling of the same type of item

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,14 +1,28 @@
 class Checkout 
 
     def initialize()
-        @items = []
+        @items = {}
     end
 
     def scan(item)
-        @items.push(item)
+        code = item[:code]
+
+        if (already_scanned?(item)) 
+            @items[code][:quantity] += 1
+        else 
+            @items[code] = {name: item[:name], price: item[:price], quantity: 1}
+        end
     end
 
     def total
-        @items.map{ |i| i[:price] }.sum
+        @items.values.reduce(0) do |total, item| 
+            total + item[:price] * item[:quantity]
+        end
     end
+
+    private
+    
+    def already_scanned?(item)
+        @items.include?(item[:code])
+    end 
 end 
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -9,4 +9,11 @@ describe Checkout do
         checkout.scan({code: "003", name: "Funky Light", price: 19.95})
         expect(checkout.total).to eq 74.20
     end 
+
+    it "creates a total for multiple of the same item" do
+        checkout.scan({code: "001", name: "Very Cheap Chair", price: 9.25})
+        checkout.scan({code: "001", name: "Very Cheap Chair", price: 9.25})
+        checkout.scan({code: "001", name: "Very Cheap Chair", price: 9.25})
+        expect(checkout.total).to eq 27.75
+    end 
 end 
